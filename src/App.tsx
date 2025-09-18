2a28ec1b043d08391e53cd40ac28052185a73991
Argovir <mralexsid@gmail.com>
2025-09-18 17:32:53 +0300

    vercel/analytics

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Analytics } from '@vercel/analytics/next';
 
 const Index = lazy(() => import("./pages/Index"));
 const BlogList = lazy(() => import("./pages/BlogList"));
@@ -29,6 +30,7 @@ const App = () => (
             <Route path="/admin" element={<AdminDashboard />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
+          <Analytics />
         </Suspense>
       </BrowserRouter>
     </TooltipProvider>
